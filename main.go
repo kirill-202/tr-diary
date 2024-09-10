81a42c8922b467c676439e0a156319a336f067b6
Kirill <kirillgoodline@gmail.com>
2024-09-10 14:36:57 +0300

    setting up index and template file getter

--- main.go
+++ main.go
@@ -6,14 +6,40 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"log"
+	"os"
 )
 
-
 const port = "8081"
+
+//This function assumes that all templates are in the same folder
+func getTemplatePaths(path string) ([]string, error){
+	var filenames []string
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, fmt.Errorf("can't read directory")
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		filenames = append(filenames, path + "/"+ entry.Name())
+	}
+	return filenames, nil
+}
+
+
 
 
 func main() {
 
+	name := "Kirill"
+	welcomeString := fmt.Sprintf("Be sure to know what you wish for %s", name)
+	templateNames, err := getTemplatePaths("./templates")
+
+	if err != nil {
+		log.Fatal("can't read templates")
+	}
 
 	if port == "" {
 		port := "8080"
@@ -26,6 +52,16 @@ func main() {
 		"message": "pong",
 	  })
 	})
+
+	router.LoadHTMLFiles(templateNames...)
+
+	router.GET("/", func(context *gin.Context){
+		context.HTML(http.StatusOK, "index.html", gin.H{
+			"title": welcomeString, 
+		})
+	})
+
+
 	fmt.Println("The test Go server is running on port: ", port)
 	router.Run(":"+ port) 
   }
