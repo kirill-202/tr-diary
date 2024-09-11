d98e89ce4406323b6411969ced7a2b99073bf7fb
Kirill <kirillgoodline@gmail.com>
2024-09-11 16:18:09 +0300

    Setting up tempaltes in Go

--- main.go
+++ main.go
@@ -6,27 +6,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"log"
-	"os"
+	//"os"
+	//"html/template"
 )
 
 const port = "8081"
 
 //This function assumes that all templates are in the same folder
-func getTemplatePaths(path string) ([]string, error){
-	var filenames []string
+// func getTemplatePaths(path string) ([]string, error){
+// 	var filenames []string
 
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		return nil, fmt.Errorf("can't read directory")
-	}
-	for _, entry := range entries {
-		if entry.IsDir() {
-			continue
-		}
-		filenames = append(filenames, path + "/"+ entry.Name())
-	}
-	return filenames, nil
-}
+// 	entries, err := os.ReadDir(path)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("can't read directory")
+// 	}
+// 	for _, entry := range entries {
+// 		if entry.IsDir() {
+// 			continue
+// 		}
+// 		filenames = append(filenames, path + "/"+ entry.Name())
+// 	}
+// 	return filenames, nil
+// }
 
 
 
@@ -35,11 +36,6 @@ func main() {
 
 	name := "Kirill"
 	welcomeString := fmt.Sprintf("Be sure to know what you wish for %s", name)
-	templateNames, err := getTemplatePaths("./templates")
-
-	if err != nil {
-		log.Fatal("can't read templates")
-	}
 
 	if port == "" {
 		port := "8080"
@@ -47,20 +43,23 @@ func main() {
 	}
 
 	router := gin.Default()
-	router.GET("/ping", func(context *gin.Context) {
-	  context.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	  })
-	})
-
-	router.LoadHTMLFiles(templateNames...)
+	router.LoadHTMLGlob("templates/*.html")
 
 	router.GET("/", func(context *gin.Context){
 		context.HTML(http.StatusOK, "index.html", gin.H{
-			"title": welcomeString, 
+			"Title": "Homepage", 
+			"Message": welcomeString,
 		})
 	})
 
+	router.GET("/about", func(c *gin.Context) {
+        data := gin.H{
+            "Title":       "About",
+            "Description": "Learn more about us on this page.",
+        }
+        c.HTML(http.StatusOK, "about.html", data)
+    })
+	
 
 	fmt.Println("The test Go server is running on port: ", port)
 	router.Run(":"+ port) 
